902d97c1e820a44da104ae1a0c4c5bd56a2be465
Mauricio Pimenta <mjbpimenta@gmail.com>
2020-11-21 00:41:56 -0300

    Adding btc commadn

--- src/App.js
+++ src/App.js
@@ -10,16 +10,20 @@ function App() {
                 backgroundColor='black'
                 barColor='black'
                 style={{fontWeight: "bold", fontSize: "1em"}}
-                // commands={{
-                //     'open-google': () => window.open('https://www.google.com/', '_blank'),
-                //     showmsg: this.showMsg,
-                //     popup: () => alert('Terminal in React')
-                // }}
-                // descriptions={{
-                //     'open-google': 'opens google.com',
-                //     showmsg: 'shows a message',
-                //     alert: 'alert', popup: 'alert'
-                // }}
+                commands={{
+                    btc: {
+                        method: (args, print, runCommand) => {
+                            print(`The price is ${args._[0] || args.color} ${runCommand}`);
+                        },
+                        options: [
+                            {
+                                name: 'color',
+                                description: 'The color the output should be',
+                                defaultValue: 'white',
+                            },
+                        ],
+                    },
+                }}
                 msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'/>
         </div>
     );
---
+++ src/command/command-validator.js
@@ -0,0 +1,9 @@
+
+
+class CommandValidator {
+
+
+
+}
+
+module.exports = {CommandValidator}
