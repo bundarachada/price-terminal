f42d7b2469c30ffae03a0f962a03ee1ac7f27000
Mauricio Pimenta <mjbpimenta@gmail.com>
2020-11-21 00:17:34 -0300

    Adding terminal in react!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Terminal from 'terminal-in-react';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+            <Terminal
+                color='green'
+                backgroundColor='black'
+                barColor='black'
+                style={{fontWeight: "bold", fontSize: "1em"}}
+                // commands={{
+                //     'open-google': () => window.open('https://www.google.com/', '_blank'),
+                //     showmsg: this.showMsg,
+                //     popup: () => alert('Terminal in React')
+                // }}
+                // descriptions={{
+                //     'open-google': 'opens google.com',
+                //     showmsg: 'shows a message',
+                //     alert: 'alert', popup: 'alert'
+                // }}
+                msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'/>
+        </div>
+    );
 }
 
 export default App;
