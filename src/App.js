7d05439281e98e2a7fc7866179bd255706ed7fa8
Mauricio Pimenta <mjbpimenta@gmail.com>
2020-11-22 01:07:04 -0300

    Changing terminal component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 import './App.css';
-import Terminal from 'react-console-emulator'
 import React from "react";
 import CommandFlow from "./command-flow";
+import Terminal from 'terminal-in-react';
 
-const terminal = React.createRef()
+// const commands = {
+//     btc: {fn: (...args) => run('btc', args)},
+//     eth: {fn: (...args) => run('eth', args)}
+// }
+//
+function run(cmd, print) {
 
-const commands = {
-    btc: {fn: (...args) => run('btc', args)},
-    eth: {fn: (...args) => run('eth', args)}
-}
-
-function run() {
-
-    const commandFlow = new CommandFlow(arguments)
+    const commandFlow = new CommandFlow(cmd)
 
     commandFlow
         .execute()
-        .then(price => terminal.current.pushToStdout(`${price}`))
-        .catch(error => terminal.current.pushToStdout(`${error}`))
-
-    return `Please wait ...`
+        .then(price => print(`${price}`))
+        .catch(error => print(`${error}`))
 }
 
 function App() {
 
     return (
-        <Terminal
-            ref={terminal}
-            commands={commands}
-            welcomeMessage={'Welcome to price terminal! Enter with command. E.g: btc'}
-            promptLabel={'>'}
-        />
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh"
+            }}>
+            <Terminal
+                color='green'
+                backgroundColor='black'
+                barColor='black'
+                style={{fontWeight: "bold", fontSize: "1em"}}
+                msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
+                commandPassThrough={(cmd, print) => {
+                    run(cmd, print)
+                }}
+            />
+        </div>
     );
 }
 
